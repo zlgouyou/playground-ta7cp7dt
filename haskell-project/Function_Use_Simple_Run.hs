5c076e8584e7b490bc019c94fa1070f6e6e5162c
Ludovic GOUYOU <ludovic.gouyou@zenika.com>
2018-05-12 14:15:51 +0200

    add function use

---
+++ haskell-project/Function_Use_Simple_Run.hs
@@ -0,0 +1,4 @@
+{-# LANGUAGE CPP #-}
+module Function_Use_Simple_Run(addUse) where
+#include "Function_Definition.hs"
+#include "Function_Use_Simple.hs"
---
+++ haskell-project/Function_Use_Simple_Test.hs
@@ -0,0 +1,17 @@
+import Function_Use_Simple_Run (addUse)
+import qualified TechIo
+import Control.Monad
+import Control.Exception
+import Data.IORef
+
+currentTest = do
+    TechIo.simpleTest addUse 3
+
+main = do
+  result <- TechIo.runner currentTest
+
+  case result of
+    TechIo.Failure -> do
+      TechIo.sendMsg "Astuce 💡" "Humain à ce rythme je vais te battre \x1F914"
+    TechIo.Success -> do
+      TechIo.sendMsg "Bien joué. 🎆" "Toujours plus vite toujours plus loin"
