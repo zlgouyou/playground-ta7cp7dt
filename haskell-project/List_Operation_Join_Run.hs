5006dc46cddffa694039626bfde02c8586603824
Ludovic GOUYOU <ludovic.gouyou@zenika.com>
2018-05-17 15:15:33 +0200

    List Opreration Join test if ++ is used

--- haskell-project/List_Operation_Join_Run.hs
+++ haskell-project/List_Operation_Join_Run.hs
@@ -1,3 +1,10 @@
 {-# LANGUAGE CPP #-}
 module List_Operation_Join_Run(joinList) where
+import qualified TechIoMockFunction as Mock
+import Prelude hiding ((++))
+import qualified GHC.Base ((++))
+
+(++) :: [a] -> [a] -> [a]
+(++) = Mock.mockFunction (GHC.Base.++)
+
 #include "List_Operation_Join.hs"
--- haskell-project/List_Operation_Join_Test.hs
+++ haskell-project/List_Operation_Join_Test.hs
@@ -1,5 +1,6 @@
 import List_Operation_Join_Run (joinList)
 import qualified TechIo
+import qualified TechIoMockFunction as Mock
 import Control.Monad
 import Control.Exception
 import Data.IORef
@@ -9,11 +10,22 @@ currentTest = do
     TechIo.simpleTest (joinList [] [1..4]) [1..4]
     TechIo.simpleTest (joinList [1..4] [5..8]) [1..8]
 
+simpleRunner tests = handle TechIo.assertHandler $ do
+            tests
+            return TechIo.Success
+
 main = do
-  result <- TechIo.runner currentTest
+  result <- simpleRunner currentTest
 
   case result of
     TechIo.Failure -> do
       TechIo.sendMsg "Astuce 💡" "Humain à ce rythme je vais te battre \x1F914"
     TechIo.Success -> do
-      TechIo.sendMsg "Bien joué. 🎆" "Toujours plus vite toujours plus loin"
+      used <- Mock.isFunctionUsed
+      case used of
+        True -> do
+          TechIo.sendMsg "Bien joué. 🎆" "Toujours plus vite toujours plus loin"
+          TechIo.success
+        False -> do
+          TechIo.sendMsg "Oops! 🐞" "N'essaye pas de m'avoir humain utilise la de concaténation de fonction!"
+          TechIo.failure
